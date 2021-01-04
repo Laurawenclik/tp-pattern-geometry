8cbc798756c6dd240cbd8102db9cd64150851fad
Laurawenclik <laurawenclik@gmail.com>
2021-01-04 15:41:49 +0100

    0.2 Correction

--- src/test/java/org/acme/geometry/TestLineString.java
+++ src/test/java/org/acme/geometry/TestLineString.java
@@ -11,7 +11,7 @@ public class TestLineString {
 	
 	@Test
 	public void testConstructor() {
-		Coordinate c = new Coordinate(5, 10.2);
+		Coordinate c = new Coordinate(1, 2);
 		Point p1 = new Point();
 		Point p2 = new Point(c);
 		List<Point> listPoint = new ArrayList<Point>();
@@ -25,7 +25,7 @@ public class TestLineString {
 	
 	@Test
 	public void testGetNumPoints() {
-		Coordinate c = new Coordinate(5, 10.2);
+		Coordinate c = new Coordinate(1, 2);
 		Point p1 = new Point();
 		Point p2 = new Point(c);
 		List<Point> listPoint = new ArrayList<Point>();
@@ -38,7 +38,7 @@ public class TestLineString {
 	
 	@Test
 	public void testGetPointN() {
-		Coordinate c = new Coordinate(5, 10.2);
+		Coordinate c = new Coordinate(1, 2);
 		Point p1 = new Point();
 		Point p2 = new Point(c);
 		List<Point> listPoint = new ArrayList<Point>();
